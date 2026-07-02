49ffbaf2de5898a5aefcf91d684a86d51026d96b
Hiago 7k <hiago7k@gmail.com>
2026-07-01 21:01:25 -0300

    praticando conversao de tipo

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String[] args){
+        // 1 Crie um programa que realize a média de duas notas decimais e exiba o resultado.
+
+        double nota1 = 8;
+        double nota2 = 6;
+        double media = (nota1 + nota2) / 2;
+
+        System.out.println("A media ds notas: " + nota1 + " e " + nota2 + " é " + media);
+
+    }
+}
