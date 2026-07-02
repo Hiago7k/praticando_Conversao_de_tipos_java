06783ed2c03750a60f1a5c6b15b81a3709fa0b49
Hiago 7k <hiago7k@gmail.com>
2026-07-01 21:05:50 -0300

    concatenacao de char e string

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,19 @@ class Main{
         System.out.println("A media ds notas: " + nota1 + " e " + nota2 + " é " + media);
 
 
-        // Declare uma variável do tipo double e uma variável do tipo int. Faça o casting da variável double para int e imprima o resultado.
+        // 2 Declare uma variável do tipo double e uma variável do tipo int. Faça o casting da variável double para int e imprima o resultado.
         double valor = 3500.00;
         System.out.println(valor);
         int valorConvertido = (int) valor;
         System.out.println(valorConvertido);
+
+        // 3 Declare uma variável do tipo char (letra) e uma variável do tipo String (palavra). Atribua valores a essas variáveis e concatene-as em uma mensagem.
+        char letra = 'A';
+        System.out.println(letra);
+
+        String frase = "é a primeira letra do alfabeto";
+        System.out.println(letra + " " + frase);
+
 
     }
 }
